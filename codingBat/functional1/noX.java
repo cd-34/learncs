d14ed940c6c0c935dc4d6ea5b2b93b3bb1ae2b3d
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-03-11 17:20:59 -0400

    finish codingbat functional1

---
+++ codingBat/functional1/noX.java
@@ -0,0 +1,14 @@
+public List<String> noX(List<String> strings) {
+  List<String> result = new ArrayList<>();
+  
+  for (String s : strings) {
+    String noX = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != 'x') {
+        noX += s.charAt(i);
+      }
+    }
+    result.add(noX);
+  }
+  return result;
+}
